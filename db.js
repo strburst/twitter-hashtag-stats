925b0f42a3386c2448babd67e7bbfb4a4183e89e
Allen Zheng <strburst@users.noreply.github.com>
2017-04-06 11:26:41 -0400

    Fix wrong config section in db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const config = require('./config');
 const yargs = require('yargs');
 
 // Set up the connection with db
-const db = new Sequelize(config.db.name, config.db.name, config.db.name, config.db);
+const db = new Sequelize(config.db.name, config.db.username, config.db.password, config.db);
 
 // Definition of the Tweets table
 const Tweet = db.define('tweets', {
