6f7d4f7077006136ea30172c63b927f2c12502af
Allen Zheng <strburst@users.noreply.github.com>
2017-04-15 15:13:45 +0000

    Add basic sass webpack configuration

--- server/js/index.js
+++ server/js/index.js
@@ -1,2 +1,4 @@
+require('../sass/main.sass');
+
 const str = 'world';
 console.log(`hello there ${str}`);
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,33 @@
  * Configuration file for webpack, the module bundler.
  */
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
+
+const extractSass = new ExtractTextPlugin({
+  filename: '[name].css',
+});
 
 module.exports = {
   entry: './server/js/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: extractSass.extract({
+          use: [
+            { loader: 'css-loader' },
+            { loader: 'sass-loader' },
+          ],
+        }),
+      },
+    ],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'server', 'dist'),
   },
+  plugins: [
+    extractSass,
+  ],
 };
