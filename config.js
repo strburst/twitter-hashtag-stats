222b80c8c0d29b30ac4058d124c83a2f823c27dd
Allen Zheng <strburst@users.noreply.github.com>
2017-04-15 13:36:38 +0000

    Refactor config checking into a function

--- config.js
+++ config.js
@@ -1,11 +1,20 @@
 /**
  * Attempt to read configuration and API tokens from custom.js(on)?, then environment variables.
  */
+ /* eslint no-console: "off" */
 
 const { ifRequire, falsyProps } = require('./util');
 const debug = require('debug')('tss:orm');
 
 const custom = ifRequire('./custom');
+
+function checkKeys(name, config) {
+  const unsetKeys = falsyProps(config);
+  if (unsetKeys.length > 0) {
+    console.error(`Configuration error: '${name}' has unset keys: ${unsetKeys.join(', ')}`);
+    process.exit(1);
+  }
+}
 
 const db = Object.assign({
   clear: true,
@@ -17,19 +26,16 @@ const db = Object.assign({
     host: process.env.DB_HOST || 'localhost',
     logging: debug,
   },
-}, custom ? custom.db : undefined);
+}, custom ? custom.db : null);
+checkKeys('db', db);
 
 const keys = Object.assign({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-}, custom ? custom.keys : undefined);
-
-const unsetKeys = falsyProps(keys);
-if (unsetKeys.length > 0) {
-  throw new Error(`Configuration error: secrets are unset: ${unsetKeys.join(', ')}`);
-}
+}, custom ? custom.keys : null);
+checkKeys('keys', keys);
 
 module.exports = {
   db,
