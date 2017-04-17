8e958c95d23a047d0021f69dc30f417e720f37a3
Allen Zheng <strburst@users.noreply.github.com>
2017-04-17 12:59:22 +0000

    Add hashtagstats prefix to hashtagstats page

--- server/js/main.js
+++ server/js/main.js
@@ -6,8 +6,12 @@ function checkCountry(result) {
   return this === result[0];
 }
 
+let h;
 function eachCountry(code) {
-  const h = JSON.parse($('#data').text());
+  if (h === undefined) {
+    h = JSON.parse($('#hashtagstats-data').text());
+  }
+
   let filtered = h.filter(checkCountry, code);
   if (filtered.length > 3) {
     filtered = filtered.slice(0, 3);
@@ -21,12 +25,11 @@ function eachCountry(code) {
 }
 
 $(() => {
-  $('#world-map').vectorMap({
+  $('#hashtagstats-map').vectorMap({
     map: 'world_mill',
     backgroundColor: '#50cc82',
     onRegionTipShow(e, el, code) {
       el.html(el.html() + eachCountry(code));
     },
   });
-  const countries = $('.jvectormap-region');
 });
